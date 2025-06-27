fdf14bc901dd1c1443c222191936abea99738663
neveranurak <anurak.k@neversitup.com>
2025-06-27 19:10:22 +0700

    fix work flow port

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get("/")
   getHello(): string {
     return this.appService.getHello();
   }
