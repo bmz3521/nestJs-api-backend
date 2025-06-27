e847d6fdd6cda5cdfc9a6924a5b43eedb7432d66
neveranurak <anurak.k@neversitup.com>
2025-06-27 18:44:39 +0700

    add swagger ui bro

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 5000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Plan กิน Plan เที่ยว API')
+    .setDescription('พยายามจะสร้าง api นัดเจอ สังสรรค์ วางแผนรวมตัวกับเพื่อนๆ')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
