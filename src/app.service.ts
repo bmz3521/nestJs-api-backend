708a6f024539af1fb9fdfa73f2c04006eee7fc3e
neveranurak <anurak.k@neversitup.com>
2025-06-27 18:19:28 +0700

    add more info users

--- src/app.service.ts
+++ src/app.service.ts
@@ -10,6 +10,7 @@ export class AppService {
       { id: 1, name: 'Benz', email: 'Benz@example.com', behavior : 'ดี' },
       { id: 2, name: 'Milo', email: 'Milo@example.com', behavior : 'ดี' },
       { id: 3, name: 'PorNahee', email: 'PorNahee@example.com', behavior : 'เหี้ย' },
+      { id: 4, name: 'Bam', email: 'Bam@example.com', behavior : 'รวย' },
     ];
   }
 }
